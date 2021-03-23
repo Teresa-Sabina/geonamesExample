afed2690d2d86fd6fd6440dce4fa66b42f3ba639
Teresa-Sabina <teresa.sabina@gmail.com>
2021-03-23 16:11:28 +0000

    Initial commit

---
+++ libs/js/script.js
@@ -0,0 +1,32 @@
+	$('#btnRun').click(function() {
+
+		$.ajax({
+			url: "libs/php/getCountryInfo.php",
+			type: 'POST',
+			dataType: 'json',
+			data: {
+				country: $('#selCountry').val(),
+				lang: $('#selLanguage').val()
+			},
+			success: function(result) {
+
+				console.log(JSON.stringify(result));
+
+				if (result.status.name == "ok") {
+
+					$('#txtContinent').html(result['data'][0]['continent']);
+					$('#txtCapital').html(result['data'][0]['capital']);
+					$('#txtLanguages').html(result['data'][0]['languages']);
+					$('#txtPopulation').html(result['data'][0]['population']);
+					$('#txtArea').html(result['data'][0]['areaInSqKm']);
+
+				}
+			
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				// your error code
+			}
+		}); 
+	
+
+	});
