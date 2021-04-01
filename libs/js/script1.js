1f4921e29814269eff4b5cd94604eba6933352c5
Teresa-Sabina <teresa.sabina@gmail.com>
2021-04-01 16:13:42 +0100

    Add files via upload

--- libs/js/script1.js
+++ libs/js/script1.js
@@ -2,6 +2,7 @@
 		alert("K");
 		$.ajax({
 			url: "libs/php/FindNearBy.php",
+			data: { country: $('#selCountry1').val() },
 			success: function(result) {
 				console.log(JSON.stringify(result));
 				if (result.status.name=="ok") {
@@ -9,7 +10,7 @@
 					$('#txtCountry').html(result["geonames"][0]['countryName']);
 					$('#txtCountryCode').html(result["geonames"][0]['countryCode']);
 					$('#txtName').html(result["geonames"][0]['name']);
-					$('#txtPlaceName').html(result["geonames"][0]['placename']);
+					$('#txtPlaceName').html(result["geonames"][0]['countryId']);
 					$('#txtLatitude').html(result["geonames"][0]['lat']);
 					$('#txtLongitude').html(result["geonames"][0]['lng']);
 				}
@@ -26,6 +27,7 @@
 	$('#btnSubmit2').click(function() { alert("OK");
 		$.ajax({
 			url: "libs/php/FindNearByPostalCode.php",
+			data: { country: $('#selCountry2').val() },
 			success: function(result) {
 				console.log(JSON.stringify(result));
 
@@ -34,7 +36,7 @@
 
 					$('#txtCountry').html(result["postalCodes"][0]['postalCode']);
 					$('#txtCountryCode').html(result["postalCodes"][0]['countryCode']);
-					$('#txtName').html(result["postalCodes"][0]['name']);
+					$('#txtName').html(result["postalCodes"][0]['distance']);
 					$('#txtPlaceName').html(result["postalCodes"][0]['placeName']);
 					$('#txtLatitude').html(result["postalCodes"][0]['lat']);
 					$('#txtLongitude').html(result["postalCodes"][0]['lng']);
@@ -51,9 +53,9 @@
 
 
 	$('#btnSubmit3').click(function() {
-alert("K");
 		$.ajax({
 			url: "libs/php/WikipediaSearch.php",
+			data: { country: $('#selCountry3').val() },
 			success: function(result) {
 				console.log(JSON.stringify(result));
 				if (result.status.name=="ok") {
@@ -61,7 +63,7 @@ alert("K");
 					$('#txtCountry').html(result["geonames"][0]['summary']);
 					$('#txtCountryCode').html(result["geonames"][0]['countryCode']);
 					$('#txtName').html(result["geonames"][0]['title']);
-					$('#txtPlaceName').html(result["geonames"][0]['placename']);
+					$('#txtPlaceName').html(result["geonames"][0]['feature']);
 					$('#txtLatitude').html(result["geonames"][0]['lat']);
 					$('#txtLongitude').html(result["geonames"][0]['lng']);
 				}
